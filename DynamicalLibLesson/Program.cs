8a3bb245aff4869b4c786a90dc06ff07cf348454
ZhaslanR <zhaslan_12@bk.ru>
2017-12-27 15:42:02 +0600

    Добавьте файлы проекта.

---
+++ DynamicalLibLesson/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace DynamicalLibLesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Assembly assembly = Assembly.LoadFile(@"C:\Users\РыспековЖ\source\repos\Reflection");
+
+            Type type = assembly.GetType("reFleCtiOn.seRviCe",true,true);
+
+            MethodInfo methodInfo = type.GetMethod("DestroyWorld");
+
+            object service = Activator.CreateInstance(type);
+
+            var result = methodInfo.Invoke(service, new object[] { "Мир будет уничтожен через ", 15 });
+
+            Console.ReadLine();
+        }
+    }
+}
